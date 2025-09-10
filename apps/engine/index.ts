b1018b2937739fe8a08e457a55e7d426c0bad477
GeneCodeSavvy <hxrsh.09@gmail.com>
2025-09-10 22:51:25 +0530

    slippage logic

--- apps/engine/index.ts
+++ apps/engine/index.ts
@@ -1,97 +1,108 @@
-// IN memory variable PRICES which gets updated in every 100ms 
-//  reads from the redis stream and updates the price 
+// IN memory variable PRICES which gets updated in every 100ms
+//  reads from the redis stream and updates the price
 import { createClient } from "redis";
 import { orderHandler, type userInfo } from "./order";
 
 type assetInfo = {
-    price : number,
-    decimal : number;
-}
+  price: number;
+  decimal: number;
+};
 
 const redisClient = createClient();
-redisClient.connect()
+redisClient.connect();
 const UPDATE_STREAM_KEY = "price_updates";
-const ORDER_STREAM_KEY = "order_stream"
+const ORDER_STREAM_KEY = "order_stream";
 
-export const prices: Record<string , assetInfo> = {};
+export const prices: Record<string, assetInfo> = {};
 
 // /TODO: function for reading that sends data to other functions further
-async function readStream(key:string ,  handler:(data:any)=>void , count?: number,){
-    //based on key read diff stream and pass in their handler functions
-    while(true){
-        const stream = await redisClient.xRead({
-            key,
-            id : "$"
-        },{
-            BLOCK : 0,
-            COUNT : count || 1,
-        })
-        if(!stream) continue;
-        //@ts-ignore
-        const stream_data = JSON.parse(stream[0].messages[0].message.data)
-        //pass the data to the handler functions
-        await handler(stream_data);
-    }
+async function readStream(
+  key: string,
+  handler: (data: any) => void,
+  count?: number
+) {
+  //based on key read diff stream and pass in their handler functions
+  while (true) {
+    const stream = await redisClient.xRead(
+      {
+        key,
+        id: "$",
+      },
+      {
+        BLOCK: 0,
+        COUNT: count || 1,
+      }
+    );
+    if (!stream) continue;
+    //@ts-ignore
+    const stream_data = JSON.parse(stream[0].messages[0].message.data);
+    //pass the data to the handler functions
+    await handler(stream_data);
+  }
 }
 
-async function persistBalance(userId : string , balance:userInfo){
-    try{
-        await redisClient.set(`balances:${userId}`, JSON.stringify(balance))
-    console.log("user-info updated")
-    }catch(e){
-        console.log(e);
-    }
-    
+async function persistBalance(userId: string, balance: userInfo) {
+  try {
+    await redisClient.set(`balances:${userId}`, JSON.stringify(balance));
+    console.log("user-info updated");
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 // xRead() -> Promise<StreamMessages[] | null>
 // xRead(
-//  streams: { 
-//              key : stream_name , 
+//  streams: {
+//              key : stream_name ,
 //              id: from_where_to_read(set to "$" if want read from last message only
 //                  or "0" if you want to replay the history)
 //            }
 //  options?: {
-//              BLOCK: how much to wait for new message (0 for indefinetly / 5000 -> 5sec), 
-//              COUNT : max no. of message to read in one call(for batching) 
-// 
+//              BLOCK: how much to wait for new message (0 for indefinetly / 5000 -> 5sec),
+//              COUNT : max no. of message to read in one call(for batching)
+//
 //             }
 // )
-// 
+//
 
-// We use while(true) and not setInterval(()=>{},100) 
+// We use while(true) and not setInterval(()=>{},100)
 // bcos it will poll in 100ms even if there is not data added to the stream wasting resources
-redisClient.on("connect", async()=>{
-    console.log("Engine is up and reading for latest price")
+redisClient.on("connect", async () => {
+  console.log("Engine is up and reading for latest price");
 
-    //updates current price
-    readStream(UPDATE_STREAM_KEY,(stream_data : any)=> {
-        for (const data of stream_data.price_updates){
-            prices[data.asset]={
-                price : data.price,
-                decimal : data.decimal
-            }
-        }
-        //here also change the users balance that he is gaining profit or loss on all orders
+  //updates current price
+  readStream(UPDATE_STREAM_KEY, (stream_data: any) => {
+    for (const data of stream_data.price_updates) {
+      prices[data.asset] = {
+        price: data.price,
+        decimal: data.decimal,
+      };
+    }
+    //here also change the users balance that he is gaining profit or loss on all orders
+    //check if loss is greater than margin
+    // loss = (currentSellPrice - orderBoughPrice) * qty * leverage
+  });
 
-    })
-
-    //order handler
-    //reads the order stream and calls the order handler
-    // readStream(ORDER_STREAM , (stream_data:any)=>{
-    // const orderData = { asset : stream_data.asset , qty:stream_data:qty }
-    // orderHandler(orderData);
-    // })
-    readStream(ORDER_STREAM_KEY, async(stream_data)=>{
-        const id = new Date().toString();
-        const orderData = {asset : stream_data.asset , qty : stream_data.qty, id}
-        const balances = await orderHandler(orderData)
-        if(!balances){
-            console.log("something happened while placing order")
-            return;
-        }
-        await persistBalance("user_1",balances)
-    })
-})
-
-
+  //order handler
+  //reads the order stream and calls the order handler
+  // readStream(ORDER_STREAM , (stream_data:any)=>{
+  // const orderData = { asset : stream_data.asset , qty:stream_data:qty }
+  // orderHandler(orderData);
+  // })
+  readStream(ORDER_STREAM_KEY, async (stream_data) => {
+    const id = new Date().toString();
+    const orderData = {
+      asset: stream_data.asset,
+      qty: stream_data.qty,
+      slippage: stream_data.slippage,
+      price: stream_data.price,
+      id,
+    };
+    const balances = await orderHandler(orderData);
+    if (!balances) {
+      console.log("something happened while placing order");
+      return;
+    }
+    await persistBalance("user_1", balances);
+  });
+});
--- apps/engine/order/index.ts
+++ apps/engine/order/index.ts
@@ -1,67 +1,84 @@
-import { prices } from ".."
+import { prices } from "..";
 
 type orderData = {
-    asset : string,
-    qty : number
-    id : string
-}
+  asset: string;
+  qty: number;
+  id: string;
+  slippage: number;
+  price: number;
+};
 
 type order = {
-    orderId : string,
-        userId : string,
-        type : string,
-        asset : string,
-        buy : number,
-        locked : number
-}
-export type userInfo={
-    tradable : number,
-    locked : number
-    orders : order[]
-}
+  orderId: string;
+  userId: string;
+  type: string;
+  asset: string;
+  buy: number;
+  locked: number;
+};
+export type userInfo = {
+  tradable: number;
+  locked: number;
+  orders: order[];
+};
 
-const balances:Record<string , userInfo> = {
-    "user_1" : {
-        tradable : 50000000, //stored in integer and not float with 4 decimal 5000USD = 10000000
-        locked : 0,
-        //this orders will be send to the frontend and there the current price will be calulated dynamically
-        orders : []
-    }
-}
+const balances: Record<string, userInfo> = {
+  user_1: {
+    tradable: 50000000, //stored in integer and not float with 4 decimal 5000USD = 10000000
+    locked: 0,
+    //this orders will be send to the frontend and there the current price will be calulated dynamically
+    orders: [],
+  },
+};
 
-const open_orders:order[] = []
+const open_orders: order[] = [];
 
-export const orderHandler = async({asset , qty , id} : orderData)=>{
-    const assetInfo= prices[asset]
-    if(!assetInfo) return; //need to send update to the another stream that backend reads
-    if(!balances["user_1"]) return;
-    const assetPrice = assetInfo.price/10**assetInfo.decimal
+export const orderHandler = async ({
+  asset,
+  qty,
+  id,
+  slippage,
+  price,
+}: orderData) => {
+  const assetInfo = prices[asset];
+  if (!assetInfo) return; //need to send update to the another stream that backend reads
+  //check slippage
+  const priceDiff = Math.abs(assetInfo.price - price);
+  const actualslippage = (priceDiff * 100) / price;
+  if (actualslippage > slippage) {
+    console.log("Value slipped more than the set slippage");
+    return;
+  }
 
-    const orderPriceFloat = (qty * assetPrice)
-    const orderPriceScaled = Math.round(orderPriceFloat * 10 **4);
+  //assetInfo.price
+  if (!balances["user_1"]) return;
+  const assetPrice = assetInfo.price / 10 ** assetInfo.decimal;
 
-    if(orderPriceScaled > balances["user_1"].tradable){
-        console.log("Insufficient Balance for order")
-        return
-    }
+  const orderPriceFloat = qty * assetPrice;
+  const orderPriceScaled = Math.round(orderPriceFloat * 10 ** 4);
 
-    balances["user_1"].tradable -= orderPriceScaled;
-    balances["user_1"].locked = orderPriceScaled;
-    const order = {
-        orderId : id,
-        userId : "1",
-        type : "long",
-        asset,
-        buy : assetInfo.price,
-        locked : orderPriceScaled
-    }
-    balances["user_1"].orders.push(order)
-    open_orders.push(order)
-    //then send this update to the order_status stream that the backend listens to
-    //using hset instead of stream because we want a snapshot,
-    console.log(`Order for ${qty} ${asset} placed successfully at ${orderPriceScaled}`)
-    console.log(`Users balance is ${balances["user_1"].tradable}`)
-    return balances["user_1"];
+  if (orderPriceScaled > balances["user_1"].tradable) {
+    console.log("Insufficient Balance for order");
+    return;
+  }
 
-}
-
+  balances["user_1"].tradable -= orderPriceScaled;
+  balances["user_1"].locked = orderPriceScaled;
+  const order = {
+    orderId: id,
+    userId: "1",
+    type: "long",
+    asset,
+    buy: assetInfo.price,
+    locked: orderPriceScaled,
+  };
+  balances["user_1"].orders.push(order);
+  open_orders.push(order);
+  //then send this update to the order_status stream that the backend listens to
+  //using hset instead of stream because we want a snapshot,
+  console.log(
+    `Order for ${qty} ${asset} placed successfully at ${orderPriceScaled}`
+  );
+  console.log(`Users balance is ${balances["user_1"].tradable}`);
+  return balances["user_1"];
+};
